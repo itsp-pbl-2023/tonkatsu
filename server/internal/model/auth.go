74e49cd02f23624163c44d5c37191f524d008b8f
SgKnt <sgknt@icloud.com>
2023-05-17 16:26:55 +0900

    AddAccount関数を追加

--- server/internal/model/auth.go
+++ server/internal/model/auth.go
@@ -1,5 +1,39 @@
 package model
 
 import (
-	// "github.com/jmoiron/sqlx"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 )
+
+func AddAccount(userName, password string) error {
+	var exists int8
+	err := db.Select(
+		&exists, 
+		`SELECT EXISTS (
+			SELECT * FROM users
+			WHERE user_name=?
+		)`,
+		userName,
+	)
+	if err != nil {
+		return nil
+	}
+
+	if exists == 1 {
+		return fmt.Errorf("User name %s is already used.", userName)
+	}
+
+	hashedPassBytes := sha256.Sum256([]byte(password))
+	hashedPassword := hex.EncodeToString(hashedPassBytes[:])
+	res, err := db.Exec(
+		`INSERT INTO users (user_name, user_password) VALUES (?, ?)`,
+		userName,
+		hashedPassword,
+	)
+
+	_ = res
+	// user_id := res.LastInsertId()
+
+	return err
+}
