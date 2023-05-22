0d77bbc87a0e8eb202f0050229de345043a8ef1d
Keita Nakajima <nana_tech@KeitanoMacBook-Air.local>
2023-05-22 18:34:10 +0900

    styled-componentsを使ってスタイルを少し変えました

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,4 +1,5 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -15,13 +16,34 @@ const Home = () => {
     <>
       <h1>ホーム</h1>
       <div>
-        <button onClick={handleClickLogin}>ログイン</button>
+        <StyledButton onClick={handleClickLogin}>ログイン</StyledButton>
       </div>
       <div>
-        <button onClick={handleClickRegister}>新規登録</button>
+        <StyledButton onClick={handleClickRegister}>新規登録</StyledButton>
       </div>
     </>
   );
 };
 
 export default Home;
+
+const StyledButton = styled.button`
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  margin: 1em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  width: 330px;
+  background-color: #f9f9f9;
+  cursor: pointer;
+  transition: border-color 0.25s;
+&:hover {
+  border-color: #646cff;
+}
+&:focus,
+&:focus-visible {
+  outline: 4px auto -webkit-focus-ring-color;
+}
+`;
