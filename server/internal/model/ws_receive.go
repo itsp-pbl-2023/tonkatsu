528f61a66d2d43f3f2b135336c72d79ca7aab4c8
SgKnt <sgknt@icloud.com>
2023-06-16 19:19:03 +0900

    clientの処理を更新

--- server/internal/model/ws_receive.go
+++ server/internal/model/ws_receive.go
@@ -1,5 +1,7 @@
 package model
 
+import "encoding/json"
+
 type WSMessageToReceive struct {
 	Command string `json:"command"`
 	Content any `json:"content,omitempty"`
@@ -8,3 +10,10 @@ type WSMessageToReceive struct {
 const (
 	WSCmdLeave = "leave"
 )
+
+// TODO
+func UnMarshalJSON(m []byte) (WSMessageToReceive, error) {
+	var message WSMessageToReceive
+	err := json.Unmarshal(m, &message)
+	return message, err
+} 
--- server/internal/room/client.go
+++ server/internal/room/client.go
@@ -2,18 +2,20 @@ package room
 
 import (
 	"sync"
+	"sync/atomic"
 	"time"
+	"tonkatsu-server/internal/model"
 
 	"github.com/gorilla/websocket"
 )
 
 type client struct {
-	id userID
-	name string
-	conn *websocket.Conn
-	sender chan<- *ClientMessage
+	id       userID
+	name     string
+	conn     *websocket.Conn
+	sender   chan<- *ClientMessage
 	receiver <-chan *RoomMessage
-	close bool
+	close    atomic.Bool
 }
 
 func newClient(
@@ -23,46 +25,84 @@ func newClient(
 	sender chan<- *ClientMessage,
 	receiver <-chan *RoomMessage,
 ) client {
-	return client {
-		id: id,
-		name: name,
-		conn: conn,
-		sender: sender,
+	new := client{
+		id:       id,
+		name:     name,
+		conn:     conn,
+		sender:   sender,
 		receiver: receiver,
-		close: false,
+		close:    atomic.Bool{},
 	}
+	new.close.Store(false)
+	return new
 }
 
 func (client *client) listenWS(wg *sync.WaitGroup) {
 	// TODO
-	// for {
-	// 	messageType, message, err := client.conn.ReadMessage()
-	// 	if err != nil {
-	// 		client.close = true
-	// 		return
-	// 	}
-	// 	if messageType != websocket.TextMessage {
-	// 		continue
-	// 	}
-	// }
-	
-	time.Sleep(time.Second * 5)
-	wg.Done()
+	defer wg.Done()
+	defer func() {
+		client.sender <- &ClientMessage{
+			Command: CmdLeaveRoom,
+			Content: nil,
+		}
+		client.close.Store(true)
+	}()
+
+	for {
+		if client.close.Load() {
+			return
+		}
+		deadline := time.Now().Add(time.Minute * 10)
+		client.conn.SetReadDeadline(deadline)
+		messageType, m, err := client.conn.ReadMessage()
+		if err != nil {
+			return
+		}
+		if messageType != websocket.TextMessage {
+			continue
+		}
+		message, err := model.UnMarshalJSON(m)
+		if err != nil {
+			return
+		}
+		
+		switch message.Command{
+		case model.WSCmdLeave:
+			return
+		}
+	}
 }
 
 func (client *client) listenRoom(wg *sync.WaitGroup) {
 	// TODO
-	// for {
-	// 	select {
-	// 	case m := <-client.receiver:
-	// 		switch m.Command {
-	// 		case CmdClose:
-	// 			client.close = true
-	// 			return
-	// 		}
-	// 	}
-	// }
+	defer wg.Done()
+	defer func() {
+		client.conn.WriteJSON(model.WSMessageToSend{
+			Command: model.WSCmdLeave,
+			Content: nil,
+		})
+		client.close.Store(true)
+	}()
 
-	time.Sleep(time.Second * 5)
-	wg.Done()
+	for {
+		if client.close.Load() {
+			return
+		}
+		select {
+		case m := <-client.receiver:
+			switch m.Command {
+			case CmdClose:
+				return
+			case CmdUsersInRoom:
+				userNames := m.Content.(UsersInRoom)
+				err := client.conn.WriteJSON(model.WSMessageToSend{
+					Command: model.WSCmdUpdateMembers,
+					Content: model.UpdateMembers{UserNames: userNames},
+				})
+				if err != nil {
+					return
+				}
+			}
+		}
+	}
 }
